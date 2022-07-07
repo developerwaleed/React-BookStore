04c9fba5b4e61f8175aba650655e8d74eeb2d19d
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-08 00:56:00 +0500

    Add bookinfo container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from './components/NavBar/NavBar';
+import Book from './components/Book/Book';
 
 function App() {
   return (
     <>
-
+    <Navbar />
+    <Book />
     </>
   );
 }
--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -4,19 +4,30 @@ import style from './Book.module.css';
 const Book = () => {
   return (
     <div className={style.container}>
-      <div className={style.book}>
+      <div className={style.bookinfo}>
         <ul>
           <li className={`${style.SchoolOf} ${style.TextStyle5}`}>Action</li>
           <li className={style.title}>The Hunger Games</li>
-          <li className={`${style.SuzanneCollins} ${style.TextStyle6}`}>Suzanne Collins</li>
+          <li className={`${style.SuzanneCollins} ${style.TextStyle6}`}>
+            Suzanne Collins
+          </li>
         </ul>
         <ul className={style.btnHolder}>
-          <button className={`${style.btnTextStyle} ${style.TextStyle6}`}>Comments</button>
+          <button className={`${style.btnTextStyle} ${style.TextStyle6}`}>
+            Comments
+          </button>
           <div className={style.divider}></div>
-          <button className={`${style.btnTextStyle} ${style.TextStyle6}`}>Remove</button>
+          <button className={`${style.btnTextStyle} ${style.TextStyle6}`}>
+            Remove
+          </button>
           <div className={style.divider}></div>
-          <button className={`${style.btnTextStyle} ${style.TextStyle6}`}>Edit</button>
+          <button className={`${style.btnTextStyle} ${style.TextStyle6}`}>
+            Edit
+          </button>
         </ul>
+      </div>
+      <div className={style.bookProgress}>
+        
       </div>
     </div>
   );
