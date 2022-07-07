be2a2311a624bfbae317975a17fd6c0cffdf1a03
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-08 02:44:36 +0500

    Add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/NavBar/NavBar';
-// import Book from './components/Book/Book';
-import BookList from './components/BookList/BookList';
 import Books from './components/Books/Books';
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Books />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="React-BookStore/">
+            <Route index element={<Books />} />
+            <Route path="CATEGORIES" element={<Books />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Book from '../Book/Book';
-import AddBook from '../AddBook/AddBook';
 
 const BookList = () => {
   return (
