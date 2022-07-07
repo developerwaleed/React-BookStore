4a601b643f2f1ec10f6bd6516ef90cb895ee44c0
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-08 03:30:39 +0500

    replaced category with author

--- src/components/AddBook/AddBook.js
+++ src/components/AddBook/AddBook.js
@@ -8,11 +8,12 @@ const AddBook = () => (
       <h2 className={style.formTitle}>ADD NEW BOOK</h2>
       <form action="./" className={style.addBookForm}>
         <input className={`${style.input} ${style.titleInput}`} type="text" placeholder="Book title" required Value />
-        <select className={`${style.input} ${style.categoryInput}`}>
+        <input className={`${style.input} ${style.titleInput}`} type="text" placeholder="Book Author" required Value />
+        {/* <select className={`${style.input} ${style.categoryInput}`}>
           <option value="Action">Action</option>
           <option value="Science">Science</option>
           <option value="Economy">Economy</option>
-        </select>
+        </select> */}
         <button type="button" className={style.btnAddBook}>ADD BOOK</button>
       </form>
     </section>
