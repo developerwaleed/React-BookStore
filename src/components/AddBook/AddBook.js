ff1fd507071d735c02f86ea8fb558ae7722ab876
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-13 01:21:24 +0500

    Use redux for addBook Function

--- src/components/AddBook/AddBook.js
+++ src/components/AddBook/AddBook.js
@@ -1,18 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addBook } from '../../redux/books/books';
 import style from './AddBook.module.css';
 
-const AddBook = () => (
-  <>
-    <div className={style.dividerHorizontal} />
-    <section className={style.Container}>
-      <h2 className={style.formTitle}>ADD NEW BOOK</h2>
-      <form action="./" className={style.addBookForm}>
-        <input className={`${style.input} ${style.titleInput}`} type="text" placeholder="Book title" required Value />
-        <input className={`${style.input} ${style.titleInput}`} type="text" placeholder="Book Author" required Value />
-        <button type="button" className={style.btnAddBook}>ADD BOOK</button>
-      </form>
-    </section>
-  </>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const bookState = useSelector((state) => state.books);
+  const [book, setBook] = useState({
+    id: bookState.length + 1,
+    title: '',
+    author: '',
+  });
+  const handleChangeinput = (e) => {
+    const fieldName = e.target.name;
+    if (fieldName === 'title') {
+      setBook({ title: e.target.value, author: book.author });
+    }
+    if (fieldName === 'author') {
+      setBook({ title: book.title, author: e.target.value });
+    }
+  };
+
+  const handleSubmitbtn = (e) => {
+    e.preventDefault();
+    dispatch(addBook(book));
+  };
+  return (
+    <>
+      <div className={style.dividerHorizontal} />
+      <section className={style.Container}>
+        <h2 className={style.formTitle}>ADD NEW BOOK</h2>
+        <form action="./" className={style.addBookForm}>
+          <input
+            className={`${style.input} ${style.titleInput}`}
+            name="title"
+            id="bookTitle"
+            type="text"
+            placeholder="Book title"
+            required
+            Value
+            onChange={handleChangeinput}
+          />
+          <input
+            className={`${style.input} ${style.titleInput}`}
+            name="author"
+            type="text"
+            placeholder="Book Author"
+            required
+            Value
+            onChange={handleChangeinput}
+          />
+          <button
+            type="button"
+            className={style.btnAddBook}
+            onClick={handleSubmitbtn}
+          >
+            ADD BOOK
+          </button>
+        </form>
+      </section>
+    </>
+  );
+};
 
 export default AddBook;
