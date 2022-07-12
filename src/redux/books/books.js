e84a0280a8ff9d883abfaf216fdb3c0f2766290e
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-12 06:01:16 +0500

    Add reducer for books.js

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -9,3 +9,17 @@ const initialState = [];
 export const addBook = (book) => ({ type: ADD_BOOK, payload: book });
 
 export const removeBook = (id) => ({ type: REMOVE_BOOK, id });
+
+// Reducers
+const bookReducer = (state = initialState, action = {}) => {
+    switch (action.type) {
+      case ADD_BOOK:
+        return [...initialState, action.payload];
+      case REMOVE_BOOK:
+        return state.filter((book) => book.id !== action.id);
+      default:
+        return state;
+    }
+  };
+  
+  export default bookReducer;
