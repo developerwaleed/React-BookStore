ec53b5b606b40e9aa1aa5a0a5a42893cb839a8b6
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-12 06:00:57 +0500

    Add action creator for books.js

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,3 +3,9 @@ const ADD_BOOK = './books/ADD_BOOK';
 const REMOVE_BOOK = './books/REMOVE_BOOK';
 
 const initialState = [];
+
+// Action Creator
+
+export const addBook = (book) => ({ type: ADD_BOOK, payload: book });
+
+export const removeBook = (id) => ({ type: REMOVE_BOOK, id });
