92f2daf8b98a4d147a80602af48a3bcebd47b264
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-12 23:42:39 +0500

    use redux in category components

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+import { checkStatus } from '../../redux/categories/categories';
 import style from './Categories.module.css';
 
-const Categories = () => (
-  <button type="button" className={style.btn}>Check Status</button>
-);
+// const actions = bindActionCreators();
+const Categories = () => {
+  const categoryStatus = useSelector((state) => state.category);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <button
+        type="button"
+        className={style.btn}
+        onClick={() => {
+          dispatch(checkStatus());
+        }}
+      >
+        Check Status
+      </button>
+      <div className={style.statusContainer}>{categoryStatus}</div>
+    </>
+  );
+};
 
 export default Categories;
