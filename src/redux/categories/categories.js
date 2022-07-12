ac755c1d024bf9752be86209b87bc88e9ced2cd4
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-12 06:02:20 +0500

    Add action creator and reducer for categories.js

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,2 +1,17 @@
 // Action Types
 const CHECK_STATUS = './categories/CHECK_STATUS';
+
+// Action Creator
+export const checkStatus = () => ({ type: CHECK_STATUS });
+
+// Reducers
+const categoriesReducer = (state = [], action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under Construction';
+    default:
+      return state;
+  }
+};
+
+export default categoriesReducer;
